142e6e39c64f1f551f3541a7e5916bcff10aca97
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-06-13 11:13:10 +0630

    nextjs14 authentication with clerk

--- academy/app/(home)/page.tsx
+++ academy/app/(home)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center mt-10 font-semibold">Home Page</h1>
+   <>
+      <h1 className="text-2xl text-center mt-10 font-semibold">Home Page</h1>
+      <UserButton afterSignOutUrl="/sign-in" />
+   </>
   );
 }
